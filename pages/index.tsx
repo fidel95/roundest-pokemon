cb13d2edcd004f4d814b29c37f3b2cd8b22f22bc
Fidel Castro <fc.fidel1995@gmail.com>
2021-12-22 21:14:39 -0500

    UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
   );
 };
 
