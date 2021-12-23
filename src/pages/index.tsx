cba0df666c477c329051668653c9205ace0580e9
Fidel Castro <fc.fidel1995@gmail.com>
2021-12-22 21:58:15 -0500

    add random pokemons id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 
 export default function Home() {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Fidel"}]);
+  const [first,second] = getOptionsForVote();
 
-  if(isLoading) return <div>Loading...</div>;
-  if(data) return <div>{data.greeting}</div>;
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">VS</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
